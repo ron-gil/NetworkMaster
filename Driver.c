a657ae7c233c9b91ad4b5716bcf212cfe85c9a38
ron <rinxmix@gmail.com>
2024-09-03 04:04:20 +0300

    added functions for creating a subLayer a filter and generating a GUID, and finally changed the StartType in the inf file so that the driver will start on boot after all other drivers have finished loading

--- Driver.c
+++ Driver.c
@@ -1,6 +1,16 @@
 #include "Driver.h"
 
-HANDLE engineHandle = NULL;   // Define the global variable
+#define GUID_FORMAT "%08lX-%04hX-%04hX-%02hhX%02hhX-%02hhX%02hhX%02hhX%02hhX%02hhX%02hhX"
+#define GUID_ARG(guid) (guid).Data1, (guid).Data2, (guid).Data3, (guid).Data4[0], (guid).Data4[1], (guid).Data4[2], (guid).Data4[3], (guid).Data4[4], (guid).Data4[5], (guid).Data4[6], (guid).Data4[7]
+
+HANDLE engineHandle = NULL;
+
+GUID subLayerGuids[MAX_SUB_LAYER_GUID];
+ULONG subLayerCount = 0;
+
+// Array to store the filter GUIDs
+GUID filterGuids[MAX_FILTERS];
+ULONG filterCount = 0;
 
 NTSTATUS
 DriverEntry(
@@ -11,30 +21,45 @@ DriverEntry(
     NTSTATUS status = STATUS_SUCCESS;
     WDF_DRIVER_CONFIG config;
 
-    // Debugging info: Start of DriverEntry
     KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "NetworkMaster: DriverEntry - Start\n"));
 
-    // Initialize the driver configuration object to register the EvtDeviceAdd callback
     WDF_DRIVER_CONFIG_INIT(&config, NetworkMasterEvtDeviceAdd);
 
-    // Create the driver object
     status = WdfDriverCreate(DriverObject, RegistryPath, WDF_NO_OBJECT_ATTRIBUTES, &config, WDF_NO_HANDLE);
     if (!NT_SUCCESS(status)) {
-        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "NetworkMaster: WdfDriverCreate failed with status 0x%X\n", status));
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "NetworkMaster: WdfDriverCreate failed with status %X\n", status));
         return status;
     }
 
-    // Initialize the WFP engine here
-    status = FwpmEngineOpen0(NULL, RPC_C_AUTHN_WINNT, NULL, NULL, &engineHandle);
-    if (!NT_SUCCESS(status)) {
-        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "NetworkMaster: FwpmEngineOpen0 failed with status 0x%X\n", status));
-        return status;
-    }
-
-    // Set the unload routine
     DriverObject->DriverUnload = DriverUnload;
 
-    // Debugging info: End of DriverEntry
+    // Initialize the WFP engine
+    status = FwpmEngineOpen0(NULL, RPC_C_AUTHN_WINNT, NULL, NULL, &engineHandle);
+    if (!NT_SUCCESS(status)) {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "NetworkMaster: FwpmEngineOpen failed with status %X\n", status));
+        return status;
+    }
+
+    status = CreateSubLayer(L"NetworkMaster Sublayer", L"Sublayer for NetworkMaster filters");
+    if (!NT_SUCCESS(status)) {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "NetworkMaster: CreateSubLayer failed with status %X\n", status));
+        return status;
+    }
+
+    status = CreateFilter(
+        &FWPM_LAYER_ALE_AUTH_RECV_ACCEPT_V4,
+        FWP_ACTION_BLOCK,
+        &subLayerGuids[0],
+        0,
+        NULL,
+        L"NetworkMaster Filter test",
+        NULL
+    );
+    if (!NT_SUCCESS(status)) {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "NetworkMaster: CreateFilter failed with status %X\n", status));
+        return status;
+    }
+
     KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "NetworkMaster: DriverEntry - End\n"));
 
     return status;
@@ -57,7 +82,7 @@ NetworkMasterEvtDeviceAdd(
     // Create the device object
     status = WdfDeviceCreate(&DeviceInit, WDF_NO_OBJECT_ATTRIBUTES, &hDevice);
     if (!NT_SUCCESS(status)) {
-        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "NetworkMaster: WdfDeviceCreate failed with status 0x%X\n", status));
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "NetworkMaster: WdfDeviceCreate failed with status %X\n", status));
         return status;
     }
 
@@ -69,14 +94,18 @@ NetworkMasterEvtDeviceAdd(
     return status;
 }
 
-VOID
-WfpCleanup()
+VOID WfpCleanup()
 {
     // Debugging info: WfpCleanup called
     KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "NetworkMaster: WfpCleanup - Start\n"));
 
     if (engineHandle) {
-        FwpmEngineClose0(engineHandle);
+        RemoveAllFilters();
+
+        FwpmSubLayerDeleteByKey(engineHandle, &subLayerGuids[0]);
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "NetworkMaster: Removed subLayer\n"));
+
+        FwpmEngineClose(engineHandle);
         KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "NetworkMaster: WfpEngineClosed\n"));
     }
 
@@ -100,3 +129,148 @@ DriverUnload(
     // Debugging info: DriverUnload finished
     KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "NetworkMaster: DriverUnload - End\n"));
 }
+
+VOID RemoveAllFilters() {
+    ULONG tempFilterCount = filterCount;
+    for (ULONG i = 0; i < tempFilterCount; ++i) {
+        RemoveFilter(filterGuids[i]);
+    }
+    if (filterCount > 0) {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "Failed to remove all filters\n"));
+    }
+    else
+    {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "NetworkMaster: Removed all filters\n"));
+    }
+}
+
+VOID RemoveFilter(const GUID filterGuid) {
+    NTSTATUS status;
+    status = FwpmFilterDeleteByKey(engineHandle, &filterGuid);
+    if (!NT_SUCCESS(status)) {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "Failed to delete filter with GUID "GUID_FORMAT" (status: %X)\n", GUID_ARG(filterGuid), status));
+    }
+    else {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "Successfully deleted filter with GUID "GUID_FORMAT"\n", GUID_ARG(filterGuid)));
+        filterCount--;
+    }
+}
+
+NTSTATUS GenerateGUID(GUID* myGuid) {
+    UINT16 tryCount = 0;
+    UINT16 const MAX_TRY_AMOUNT = 5;
+    // Generate a GUID
+    NTSTATUS status = ExUuidCreate(myGuid);
+    while (status == STATUS_RETRY && tryCount < MAX_TRY_AMOUNT) {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "NetworkMaster: UuidCreate retrying...\n"));
+        status = ExUuidCreate(myGuid);
+        tryCount++;
+    }
+    if (!NT_SUCCESS(status)) {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "NetworkMaster: UuidCreate failed with status %X\n", status));
+        return STATUS_UNSUCCESSFUL;
+    }
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS CreateFilter(
+    const GUID* layerKey,
+    FWP_ACTION_TYPE actionType,
+    const GUID* subLayerKey,
+    UINT32 numConditions,
+    FWPM_FILTER_CONDITION0* conditions,
+    wchar_t* name,
+    wchar_t* description
+) {
+    // Add filter to block traffic on IP V4 for all applications.
+    FWPM_FILTER0 fwpFilter;
+    RtlZeroMemory(&fwpFilter, sizeof(FWPM_FILTER0));
+
+    GUID filterKey;
+    NTSTATUS status = GenerateGUID(&filterKey);
+
+    if (!NT_SUCCESS(status)) {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "NetworkMaster: CreateFilter failed calling GenerateGUID with status %X\n", status));
+        return status;
+    }
+
+    fwpFilter.filterKey = filterKey;
+    //fwpFilter.layerKey = FWPM_LAYER_ALE_AUTH_RECV_ACCEPT_V4;
+    fwpFilter.layerKey = *layerKey;
+    //fwpFilter.action.type = FWP_ACTION_PERMIT;
+    fwpFilter.action.type = actionType;
+    //fwpFilter.subLayerKey = subLayer.subLayerKey;
+    fwpFilter.subLayerKey = *subLayerKey;
+    fwpFilter.weight.type = FWP_EMPTY; // auto-weight
+    //fwpFilter.numFilterConditions = 0; // applies to all traffic
+    fwpFilter.numFilterConditions = numConditions; // applies to all traffic
+    fwpFilter.filterCondition = conditions;
+
+    fwpFilter.displayData.name = name;
+    fwpFilter.displayData.description = description;
+
+    status = FwpmFilterAdd0(engineHandle, &fwpFilter, NULL, NULL);
+    if (!NT_SUCCESS(status)) {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "NetworkMaster: FwpmFilterAdd0 failed with status %X\n", status));
+        return status;
+    }
+
+    if (filterCount >= MAX_FILTERS) {
+        status = STATUS_UNSUCCESSFUL;
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "NetworkMaster: Adding filter failed - Max filters reached\n"));
+        return status;
+    }
+
+    filterGuids[filterCount++] = filterKey;
+
+    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "NetworkMaster: Added filter\n"));
+
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS CreateSubLayer(wchar_t* name, wchar_t* description) {
+    // Define and add the sublayer
+    FWPM_SUBLAYER0 subLayer;
+    RtlZeroMemory(&subLayer, sizeof(FWPM_SUBLAYER0));
+    
+    GUID subLayerKey;
+    NTSTATUS status = GenerateGUID(&subLayerKey);
+
+    if (!NT_SUCCESS(status)) {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "NetworkMaster: CreateSubLayer failed calling GenerateGUID with status %X\n", status));
+        return status;
+    }
+
+    subLayer.subLayerKey = subLayerKey;
+    //subLayer.displayData.name = L"NetworkMaster Sublayer";
+    subLayer.displayData.name = name;
+    //subLayer.displayData.description = L"Sublayer for NetworkMaster filters";
+    subLayer.displayData.description = description;
+    subLayer.flags = 0;
+    subLayer.weight = 0x100;
+
+    status = FwpmSubLayerAdd0(engineHandle, &subLayer, NULL);
+    if (!NT_SUCCESS(status)) {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "NetworkMaster: FwpmSubLayerAdd failed with status %X\n", status));
+        return status;
+    }
+
+    subLayerGuids[subLayerCount++] = subLayerKey;
+
+    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "NetworkMaster: Added subLayer\n"));
+
+    return STATUS_SUCCESS;
+}
+
+/*##################################
+structure of NetworkMaster wfp driver
+
+- provider of NetworkMaster
+    - subLayer for outbound traffic
+        - filter
+        - filter
+    - subLayer for inbound traffic
+        - filter
+        - filter
+
+####################################*/
--- Driver.h
+++ Driver.h
@@ -1,12 +1,40 @@
 #include <ntddk.h>
-#include <wdf.h>
+#include <initguid.h>
 #include <fwpmk.h>
+#include <wdf.h>
 
 // Global variables
 extern HANDLE engineHandle;
 
-// Function prototypes
+#define MAX_SUB_LAYER_GUID 4
+
+extern GUID subLayerGuids[MAX_SUB_LAYER_GUID];
+extern ULONG subLayerCount;
+
+// Maximum number of filters that can be stored
+#define MAX_FILTERS 10
+
+// Array to store filter GUIDs
+extern GUID filterGuids[MAX_FILTERS];
+extern ULONG filterCount;
+
+// Driver-related function declarations
 DRIVER_INITIALIZE DriverEntry;
 EVT_WDF_DRIVER_DEVICE_ADD NetworkMasterEvtDeviceAdd;
 VOID DriverUnload(_In_ PDRIVER_OBJECT DriverObject);
+
 VOID WfpCleanup();
+VOID RemoveAllFilters();
+VOID RemoveFilter(const GUID filterGuid);
+NTSTATUS GenerateGUID(GUID* myGuid);
+
+NTSTATUS CreateFilter(
+    const GUID* layerKey,
+    FWP_ACTION_TYPE actionType,
+    const GUID* subLayerKey,
+    UINT32 numConditions,
+    FWPM_FILTER_CONDITION0* conditions,
+    wchar_t* name,
+    wchar_t* description
+);
+NTSTATUS CreateSubLayer(wchar_t* name, wchar_t* description);
